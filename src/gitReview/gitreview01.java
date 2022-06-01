9b92a22fa5023f78f212fafaf7cded1e99fd7781
oleg-shin <qaolegs@gmail.com>
2022-05-31 21:34:04 -0400

    1st commit

---
+++ src/gitReview/gitreview01.java
@@ -0,0 +1,10 @@
+package gitReview;
+
+public class gitreview01 {
+
+	public static void main(String[] args) {
+		System.out.println("today we learn github");
+		System.out.println("today is very good day");
+	}
+
+}

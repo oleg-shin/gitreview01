2aa88dc6731ac55f72e3c23275b687cb02623597
oleg-shin <qaolegs@gmail.com>
2022-05-31 21:52:00 -0400

    2nd commit

--- src/gitReview/gitreview01.java
+++ src/gitReview/gitreview01.java
@@ -5,6 +5,7 @@ public class gitreview01 {
 	public static void main(String[] args) {
 		System.out.println("today we learn github");
 		System.out.println("today is very good day");
+		System.out.println("sdasd");
 	}
 
 }
